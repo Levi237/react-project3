33b25631cb66583ba639baf21c71f0f9b598c34e
Levi Winans <levieiko@gmail.com>
2019-05-16 15:30:02 -0700

    add static json

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
  
   getAlerts = async () => {
     try {
-      const alerts = await fetch('https://developer.nps.gov/api/v1/alerts&limit=50?api_key=WZ7TKRUSuVC5NEf18Txpco74bA3qKdFBZqxfq9W6')
+      const alerts = await fetch(`https://developer.nps.gov/api/v1/alerts&limit=50?api_key=${process.env.NPS_API_KEY}`)
       const alertsJson = await alerts.json();
         return alertsJson
     } catch(err) {
@@ -92,7 +92,7 @@ class App extends Component {
 
   getParkNames = async () => {
     try {
-      const parkNames = await fetch('https://developer.nps.gov/api/v1/parks&limit=50?api_key=WZ7TKRUSuVC5NEf18Txpco74bA3qKdFBZqxfq9W6')
+      const parkNames = await fetch(`https://developer.nps.gov/api/v1/parks&limit=50?api_key=${process.env.NPS_API_KEY}`)
       const nameJson = await parkNames.json();
         return nameJson
     } catch(err) {
