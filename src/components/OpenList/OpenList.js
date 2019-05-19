d41dd599939eb31c1d06e3e1f04406cb66568071
Levi Winans <levieiko@gmail.com>
2019-05-19 16:59:16 -0700

    failed attempt, ! misplaced

--- src/components/OpenList/OpenList.js
+++ src/components/OpenList/OpenList.js
@@ -15,10 +15,10 @@ class OpenList extends Component {
         let compareClosureList = this.props.closureList;
         for (let u = 0; u < currentUserList.length; u++){
             for (let c = 0; c < compareClosureList.length; c++){
-                if (!currentUserList[u].id === compareClosureList[c].id){
+                if (currentUserList[u].id !== compareClosureList[c].id){
                     newList.unshift(currentUserList[u])
                     // console.log(compareClosureList[c].id, '<======= match found =======>', currentUserList[u].id)
-                    console.log(currentUserList[u].id, '<======================= nothing should show up =======================>', compareClosureList[c].id)
+                    console.log(currentUserList[u].id, '<========= currentUser ============== nothing should show up ============ closures ===========>', compareClosureList[c].id)
                 }
             }
             console.log(newList, "<=============attempt push to new list")
