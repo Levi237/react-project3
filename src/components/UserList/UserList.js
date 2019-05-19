50c65f1eb21e9bc3b94f84a1e2fc8f88a35ac757
Levi Winans <levieiko@gmail.com>
2019-05-19 15:42:46 -0700

    onclick this.props handleSetmap

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -8,14 +8,14 @@ class UserList extends Component {
     }
 
     render (){
-        const { currentUser, deleteItem } = this.props
+        const { currentUser, deleteItem, handleSetMap } = this.props
         const myList = currentUser.userList.map((alert, i) => 
             <section className="listItem" key={i}>
                 <strong>
                     <div>{alert.fullName}</div>
                     <a className="listAlink" href={alert.url} target="_blank" rel="noopener noreferrer">{alert.title}{alert.name}</a>
                 </strong><br />
-                <form className="mapBtn" onSubmit={this.showOnMap}><button onClick={this.props.handleSetMap} value={alert.latLong}>Map</button></form><button className="userListButton" onClick={deleteItem.bind(null, alert._id, currentUser._id)}>Remove from List</button>
+                <form className="mapBtn" onSubmit={this.showOnMap}><button onClick={handleSetMap} value={alert.latLong}>Map</button></form><button className="userListButton" onClick={deleteItem.bind(null, alert._id, currentUser._id)}>Remove from List</button>
             </section>
         )
         return (
