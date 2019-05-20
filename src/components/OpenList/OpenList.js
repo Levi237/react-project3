e1d12cd47592fe50064acf5bc86ab11a57c66e7b
Levi Winans <levieiko@gmail.com>
2019-05-19 17:34:46 -0700

    test

--- src/components/OpenList/OpenList.js
+++ src/components/OpenList/OpenList.js
@@ -16,7 +16,7 @@ class OpenList extends Component {
         for (let u = 0; u < currentUserList.length; u++){
             let loopUser = currentUserList[u]
             for (let c = 0; c < compareClosureList.length; c++){
-                if (loopUser.id !== compareClosureList[c].id){
+                if (loopUser.id === compareClosureList[c].id){
                     newList.unshift(loopUser)
                     // console.log(compareClosureList[c].id, '<======= match found =======>', currentUserList[u].id)
                     console.log(loopUser.id, '<========= currentUser ============== nothing should show up ============ closures ===========>', compareClosureList[c].id)
