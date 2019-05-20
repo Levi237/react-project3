c82ca5e5868aa956a05a98ee41dab53b28b0d712
Levi Winans <levieiko@gmail.com>
2019-05-20 12:49:03 -0700

    rearrange userlist

--- src/components/OpenList/OpenList.js
+++ src/components/OpenList/OpenList.js
@@ -46,7 +46,7 @@ class OpenList extends Component {
 
         return(
             <div>
-                <h2>Hello {this.currentUser.username}</h2>
+                <h2>Hello {this.props.currentUser.username}</h2>
                 { showOpen.length > 0 &&
                 <div><h1>Now Open</h1>{showOpen}</div>
                 }
