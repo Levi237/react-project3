71e3d948a20e954b5d24543d3283bd67086282df
Levi Winans <levieiko@gmail.com>
2019-05-20 21:30:41 -0700

    style

--- src/components/OpenList/OpenList.js
+++ src/components/OpenList/OpenList.js
@@ -10,33 +10,18 @@ class OpenList extends Component {
         let currentUserList = this.props.currentUser.userList;
         let compareClosureList = this.props.closureList;
         
-        // console.log(this.props.currentUser, "<================this.props.currentUser");
-        // console.log(this.props.currentUser.userList, "<================this.props.currentUser.userList");
-        // console.log(this.props.closureList, "<============================== this.props.closureList");
-        
         let loopUser = [...currentUserList];
         //Array.from(currentuserList) // also works
-        // for (let u = 0; u < currentUserList.length; u++){
-            // loopUser.push(...currentUserList)
-            // console.log("push into userLoop ---->", currentUserList[u].title)
-        // }
 
         for (let c = 0; c < compareClosureList.length; c++){
             for (let l = loopUser.length - 1; l >= 0; l--){
-            // for (let l = 0; l < loopUser.length; l++){
+
 //--------------------> POSSIBLE GLITCH, MIGHT NEED TO REMOVE FROM OPENLIST, HOPE JUST REMOVES WHEN DELETED
-                // console.log(loopUser[l].fullName, '<========loopUser[l].fullName')
-                // console.log(compareClosureList[c].fullName, '<=======================compareClosureList[c].fullName')
                 if (loopUser[l].id === compareClosureList[c].id){
-                    // console.log("match", loopUser[l].fullName, '<======= match found =======>',compareClosureList[c].fullName)
                     loopUser.splice(l, 1);
                 }
             }
         }        
-        // console.log(loopUser, "<=============loopUser splice()")
-        // console.log(currentUserList, "<=============currentUser.userList")
-
-
         this.setState({
             openList: loopUser
         })
@@ -75,31 +60,3 @@ class OpenList extends Component {
 }
 
 export default OpenList
-
-
-    // this.getAlerts().then(alerts => {
-    //     this.getParkNames()
-    //       .then(names => {
-    //         const filterAlerts = alerts.data.filter(a => (a.category === "Park Closure" && !a.title.includes("Restrooms") && a.description.includes("closed" || "closure")))
-        
-
-
-
-
-    //        const list = filterAlerts.reduce((total, f) => {
-    //           names.data.forEach(a => {
-    //             if(a.parkCode === f.parkCode) {
-    //               total.push(Object.assign(f, a))
-    //               return total
-    //             }
-    //           })
-    //           return total
-    //         }, [])
-
-    //     this.setState({
-    //         closureList: list,
-    //         loading: false
-    //       })
-    //     })
-    // })
-    // }
