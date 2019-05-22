ad466ed721615f1a6db7f53579271f56dbffd12d
Levi Winans <levieiko@gmail.com>
2019-05-21 20:10:22 -0700

    check new switch and nav

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,5 +2,6 @@ export const ROOT = '/';
 export const HOME = '/home';
 export const REGISTER = '/register';
 export const LOGIN = '/login';
-export const TRACKER = '/:id/tracker';
+export const USERS = '/users';
 export const LOGOUT = '/';
+export const TRACKER = '/:id/tracker';
