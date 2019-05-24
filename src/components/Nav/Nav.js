8e39afc0199f1fe2c2e8175e8e571d863404ff52
Levi Winans <levieiko@gmail.com>
2019-05-24 09:37:40 -0700

    decorate list

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,17 +6,17 @@ import './Nav.css'
 
 const Nav = ({currentUser, logoutUser}) => 
 <div className="navBox">
-    <NavLink activeClassName="active" className="navBtn" to={routes.HOME}>HOME</NavLink>
+    <NavLink activeClassName="underline" className="navBtn" to={routes.HOME}>HOME</NavLink>
     { currentUser 
-        ? <NavLink activeClassName="active" className="navBtn" to={routes.TRACK} >TRACK</NavLink> 
-        : <NavLink activeClassName="active" className="navBtn" to={routes.REGISTER} >JOIN</NavLink>
+        ? <NavLink activeClassName="underline" className="navBtn" to={routes.TRACK} >TRACK</NavLink> 
+        : <NavLink activeClassName="underline" className="navBtn" to={routes.REGISTER} >JOIN</NavLink>
     }
     { currentUser &&
-        <NavLink activeClassName="active" className="navBtn" to={routes.SEARCH}>SEARCH</NavLink>
+        <NavLink activeClassName="underline" className="navBtn" to={routes.SEARCH}>SEARCH</NavLink>
     }
     { currentUser 
         ? <NavLink className="navBtn" to={routes.LOGOUT} onClick={logoutUser}>LOGOUT</NavLink> 
-        : <NavLink activeClassName="active" className="navBtn" to={routes.LOGIN}>LOGIN</NavLink>
+        : <NavLink activeClassName="underline" className="navBtn" to={routes.LOGIN}>LOGIN</NavLink>
     }
 
 </div>
