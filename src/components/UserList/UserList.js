4d669477410ca2cbf47cec7a785e994f08fc9404
Levi Winans <levieiko@gmail.com>
2019-05-24 22:11:39 -0700

    try to add green pin

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -53,12 +53,9 @@ class UserList extends Component {
                     <strong>
                         { checkList.map((check) => check.title === alert.title
                         ? <div className="alertOpen">NOW OPEN</div>
-                        : ''
+                        : <div className="currentlyClosed">CURRENTLY CLOSED</div>
                         )}
-                        { checkList.map((check) => check.title === ![...alert.title]
-                        ? <div className="currentlyClosed">CURRENTLY CLOSED</div>
-                        : ''
-                        )}
+
 
                         <div>{alert.fullName}</div>
                         <a className="listAlink" href={alert.url} target="_blank" rel="noopener noreferrer">{alert.title}{alert.name}</a>
