bb2f3d16a83c3dcf179db8d0ee20756e9c97e8f7
Levi Winans <levieiko@gmail.com>
2019-05-27 22:35:50 -0700

    still working on remove button

--- src/components/Alerts/Alerts.js
+++ src/components/Alerts/Alerts.js
@@ -49,15 +49,18 @@ class Alerts extends Component {
                         <div className="details">
                             {park.description}
                         </div>
+                        <div>
+
                         <form onSubmit={(event) => this.doAddAlert(event, park)}>
                         {/* { currentUser.userList.map((check) => check.title === alert.title
                             ? <div className="alertOpen">NOW OPEN</div>
-                            : ''  )}*/}
+                        : ''  )}*/}
                     
                         { currentUser && 
                         <button className="alertsButton" type="submit">Add to List</button>             
-                        }
-                    </form>
+                    }
+                        </form>
+                        </div>
                     </div>
 
                 </section>  
