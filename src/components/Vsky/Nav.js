36b20ac82669581fec24a5e1447f240b89404865
Levi Winans <levieiko@gmail.com>
2019-06-02 17:16:28 -0700

    virtsky enlarge

--- src/components/Vsky/Nav.js
+++ src/components/Vsky/Nav.js
@@ -33,65 +33,82 @@ class Vnav extends Component {
             <div className="vskyContainer">
 
                 <form>
-                Constellation: 
-                <select onChange={changeHandler} name='constellations' value={constellations}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select>
-                <br />
-                Constellation Names: 
-                <select onChange={changeHandler} name='constellationLabels' value={constellationLabels}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select> 
-                <br />
-                Gridlines: 
-                <select onChange={changeHandler} name='azimuthGridlines' value={azimuthGridlines}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select> 
-                <br />
-                Meridian: 
-                <select onChange={changeHandler} name='showMeridian' value={showMeridian}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select> 
-                <br />
-                Star Names: 
-                <select onChange={changeHandler} name='showStarLabels' value={showStarLabels}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select>                     
-                <br />
-                Planets: 
-                <select onChange={changeHandler} name='showPlanets' value={showPlanets}>
-                        <option value="true">On</option>
-                        <option value="false">Off</option>
-                </select> 
-                <br />
-                Planet Names: 
-                <select onChange={changeHandler} name='showPlanetLabels' value={showPlanetLabels}>
-                        <option value="true">On</option>
-                        <option value="false">Off</option>
-                </select> 
-                <br />
-                Planet Orbits: 
-                <select onChange={changeHandler} name='showOrbits' value={showOrbits}>
-                        <option value="false">Off</option>
-                        <option value="true">On</option>
-                </select>  
-                <br />
-                View: 
-                <select onChange={changeHandler} name='projection' value={projection}>
-                        <option value="stereo">Stereo</option>
-                        <option value="polar">polar</option>
-                        <option value="lambert">lambert</option>
-                        <option value="equirectaungular">equirectaungular</option>
-                        <option value="mollweide">mollweide</option>
-                        <option value="planechart">planechart</option>
-                        <option value="gnomic">gnomic</option>
-                </select>                                     
-
+                    <section>
+                        Constellation: 
+                        <select onChange={changeHandler} name='constellations' value={constellations}>
+                            <option value="false">Off</option>
+                            <option value="true">On</option>
+                        </select>
+                    </section>                    
+                    <br />
+                    <section>
+                        Constellation Names: 
+                        <select onChange={changeHandler} name='constellationLabels' value={constellationLabels}>
+                                <option value="false">Off</option>
+                                <option value="true">On</option>
+                        </select> 
+                    </section>
+                    <br />
+                    <section>
+                        Gridlines: 
+                        <select onChange={changeHandler} name='azimuthGridlines' value={azimuthGridlines}>
+                                <option value="false">Off</option>
+                                <option value="true">On</option>
+                        </select> 
+                    </section>
+                    <br />
+                    <section>
+                        Meridian: 
+                        <select onChange={changeHandler} name='showMeridian' value={showMeridian}>
+                                <option value="false">Off</option>
+                                <option value="true">On</option>
+                        </select> 
+                    </section>
+                    <br />
+                    <section>
+                        Star Names: 
+                        <select onChange={changeHandler} name='showStarLabels' value={showStarLabels}>
+                                <option value="false">Off</option>
+                                <option value="true">On</option>
+                        </select>                     
+                    </section>
+                    <br />
+                    <section>
+                        Planets: 
+                        <select onChange={changeHandler} name='showPlanets' value={showPlanets}>
+                                <option value="true">On</option>
+                                <option value="false">Off</option>
+                        </select> 
+                    </section>
+                    <br />
+                    <section>
+                        Planet Names: 
+                        <select onChange={changeHandler} name='showPlanetLabels' value={showPlanetLabels}>
+                                <option value="true">On</option>
+                                <option value="false">Off</option>
+                        </select> 
+                    </section>
+                    <br />
+                    <section>
+                        Planet Orbits: 
+                        <select onChange={changeHandler} name='showOrbits' value={showOrbits}>
+                                <option value="false">Off</option>
+                                <option value="true">On</option>
+                        </select>  
+                    </section>
+                    <br />
+                    <section>
+                        View: 
+                        <select onChange={changeHandler} name='projection' value={projection}>
+                            <option value="stereo">Stereo</option>
+                            <option value="polar">polar</option>
+                            <option value="lambert">lambert</option>
+                            <option value="equirectaungular">equirectaungular</option> 
+                            <option value="mollweide">mollweide</option>
+                            <option value="planechart">planechart</option>
+                            <option value="gnomic">gnomic</option>
+                        </select>      
+                    </section>                               
                 </form>
                         
             </div>
