2a1c60973eeaea627af0c1a6c87004c04057ce9c
Levi Winans <levieiko@gmail.com>
2019-06-04 14:39:57 -0700

    working on edit

--- src/components/EditUser/EditUser.js
+++ src/components/EditUser/EditUser.js
@@ -92,17 +92,17 @@ export class EditUser extends Component {
         console.log(this.state.username, this.state.password)
 
         // const { username, password } = this.state
-        // const { submitEditUser, changeHandler } = this.props
+        const { currentUser } = this.props
 
         return(
 
             <section  className="enter">
                 <form onSubmit={e => this.submitEditUser(e)}>
-                    <input type="text" placeholder={this.props.currentUser.username} name="username" onChange={this.changeHandler}/>
+                    <input type="text" placeholder={currentUser.username} name="username" onChange={this.changeHandler}/>
                     <button type="submit">Edit User</button>
                 </form>
                 <form onSubmit={e => this.submitEditUser(e)}>
-                    <input type="password" placeholder="Your Password" name="password" onChange={this.changeHandler()} value={password}/>
+                    <input type="password" placeholder="Your Password" name="password" onChange={this.changeHandler} value={password}/>
                     <button type="submit">Edit Password</button>
                 </form>
             </section> 
@@ -110,42 +110,5 @@ export class EditUser extends Component {
     }
 }
 
-//   closeAndEdit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-
-//       const editResponse = await fetch('http://localhost:9000/api/v1/movies/' + this.state.movieToEdit._id, {
-//         method: 'PUT',
-//         body: JSON.stringify(this.state.movieToEdit),
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       })
-
-//       const parsedResponse = await editResponse.json();
-
-
-//       const editedMovieArray = this.state.movies.map((movie) => {
-
-//         if(movie._id === this.state.movieToEdit._id){
-
-//             movie = parsedResponse.data;
-
-//         }
-
-//         return movie
-//       });
-
-
-//       this.setState({
-//         movies: editedMovieArray,
-//         modalShowing: false
-//       });
-
-//     }catch(err){
-//       console.log(err);
-//     }
-//   }
 
 export default EditUser
