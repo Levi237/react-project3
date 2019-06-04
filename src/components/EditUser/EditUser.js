af1583d1cb4936f9f096f1221a3c5e7babe3ee99
Levi Winans <levieiko@gmail.com>
2019-06-04 13:06:21 -0700

    basics done push to heroku

--- src/components/EditUser/EditUser.js
+++ src/components/EditUser/EditUser.js
@@ -15,29 +15,59 @@ export class EditUser extends Component {
     console.log(this.state.username, this.state.password, "<--------changeHandler")
     }
 
+    // editUser = async () => {
+    //     try { const editUserResponse = await fetch(`/users/update/${this.props.match.params.id}`, {
+    //       method: 'PUT',
+    //       credentials: 'include',
+    //       body: JSON.stringify(this.state.userToEdit),
+    //       headers: {
+    //         'Content-Type': 'application/json'
+    //       }
+          
+    //     })
+    //     console.log(editUserResponse)
+    //     const editUserJson = await editUserResponse.json();
+    //     console.log(editUserJson)
+    //     this.setState({
+    //       user: editUserJson
+    //     },()=>{
+    //       this.props.doSetCurrentUser(editUserJson)
+    //     })
+          
+    //     } catch(err){
+    //       console.log(err)
+    //     }
+    //   }
+
+
     submitEditUser = async (e) => {
         e.preventDefault();
         console.log(this.state.username, '<---------this.state.username===============<<<<<<')
         console.log(this.state, "<--------------this.state")
         console.log(e.target.username, '<-----------e.target.username');
         let userid = this.props.currentUser
-          const editUser = await fetch(process.env.REACT_APP_API+'/api/v1/'+userid._id+'/edit', {
-            method: 'PUT',
-            credentials: 'include',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Content-type' : 'application/json'
-            }
-          })
-          console.log(editUser, "<-----------editUser in editUser")
-          const parsedResponse = await editUser.json();
-          console.log(parsedResponse, "<==========parsed response")
-          if(parsedResponse.data) {
-              this.props.doSetCurrentUser(parsedResponse.data)
-                  this.setState({
-                      logged: true,
-                  })
-          }
+        try {
+            const editUser = await fetch(process.env.REACT_APP_API+'/api/v1/'+userid._id+'/edit', {
+                method: 'PUT',
+                credentials: 'include',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Content-type' : 'application/json'
+                }
+              })
+              console.log(editUser, "<-----------editUser in editUser")
+              const parsedResponse = await editUser.json();
+              console.log(parsedResponse, "<==========parsed response")
+              if(parsedResponse.data) {
+                  this.props.doSetCurrentUser(parsedResponse.data)
+                      this.setState({
+                          logged: true,
+                      })
+              }
+        } catch(err){
+            console.log(err)
+        }
+
       }
     // export function updateBlogPost(id, data) {
     //     return fetch('http://api.symfony-3.dev/app_dev.php/posts/' + id, {
@@ -81,19 +111,19 @@ export class EditUser extends Component {
         console.log(this.state.username, this.state.password)
 
         const { username, password } = this.state
-        const { changeHandler, submitEditUser } = this.props
+        // const { submitEditUser, changeHandler } = this.props
 
         return(
 
             <section  className="enter">
-                <form onSubmit={e => submitEditUser(e)}>
-                    <input type="text" placeholder="Your Name Here" name="username" onChange={e => changeHandler(e)} value={username}/>
+                <form onSubmit={e => this.submitEditUser(e)}>
+                    <input type="text" placeholder="Your Name Here" name="username" onChange={this.changeHandler()} value={username}/>
                     <button type="submit">Edit User</button>
                 </form>
-                <form onSubmit={e => submitEditUser(e)}>
-                    <input type="password" placeholder="Your Password" name="password" onChange={e => changeHandler(e)} value={password}/>
+                {/* <form onSubmit={e => this.submitEditUser(e)}>
+                    <input type="password" placeholder="Your Password" name="password" onChange={this.changeHandler()} value={password}/>
                     <button type="submit">Edit Password</button>
-                </form>
+                </form> */}
             </section> 
         )
     }
