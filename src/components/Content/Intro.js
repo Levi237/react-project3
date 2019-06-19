7e0a45d418ee3f9f21c443536d652b86df0aeba0
Levi Winans <levieiko@gmail.com>
2019-06-19 16:52:55 -0700

    add r to infomation

--- src/components/Content/Intro.js
+++ src/components/Content/Intro.js
@@ -7,7 +7,7 @@ const Intro = () =>
         <br />
         <h1>Origins</h1>
             <p>With outdoor activities such as camping and back-packing gaining in popularity, it has become increasingly difficult to obtain access permits to federal parks.  On average, reservations to popular wilderness zones often need to be made six months in advance. However, unpredictable natural disasters such as fires and mudslides that cause park closures can compromise even the most well-planned trips.  Not knowing when the park will re-open is an additional obstacle to planning the perfect outdoor adventure.</p>
-            <p>The original intent of this website was to provide the public with information about parks’ statuses.  Not only would a user be able to search for current park closures and openings, but also sort through data to easily store, track, and retrieve infomation specific to their selected parks and assist with their planning. Since then more content has been added to enhance the user experience.</p>
+            <p>The original intent of this website was to provide the public with information about parks’ statuses.  Not only would a user be able to search for current park closures and openings, but also sort through data to easily store, track, and retrieve information specific to their selected parks and assist with their planning. Since then more content has been added to enhance the user experience.</p>
     </>
 
 export default Intro
